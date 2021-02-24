b25d901fd11ac39d998987cd003fa79660e35667
Jorge Vandereyken <jorgevandereyken@gmail.com>
2021-02-24 14:10:54 +0100

    commit 4

--- challenge-js-files/graph.js
+++ challenge-js-files/graph.js
@@ -1,36 +1,84 @@
-var ctx = document.getElementById('myChart');
 
-var Crime= [1073,121,305,441,5997,41,194,2269,72,2819,8,50,75,37,472,16,1139,548,1119,403,90,90,425,1402,12,1,274,750,6,30,96,1904];
-var Countries = ['Belgium','Bulgaria','Czech Republic','Denmark','Germany','Estonia','Ireland','Greece','Spain','France','Croatia','Italy','Cyprus','Latvia','Lithuania','Luxembourg','Hungary','Malta','The Netherlands','Austria','Poland','Portugal','Romania','Slovenia','Slovakia','Finland','Sweden','Iceland','Liechtenstein','Norway','Switzerland','Montenegro','Macedonia','Serbia','Turkey'];
-
-var myChart = new Chart(ctx, {
- type: 'bar',
- data: {
-    labels: Countries,
-    datasets: [{
-        label: 'Crime Rate 2012',
-        backgroundColor: 'rgb(255, 99, 132)',
-        borderColor: 'rgb(255, 99, 132)',
-        data: Crime
-        
-
+window.onload = function() { 
  
-
-    }];
- },
- maintainAspectRatio: false,
- responsive: true
-});
-
-
-$.ajax({
-    url: "prison.csv",
-    async: false,
-    success: function (csvd) {
-        data = $.csv.toArrays(csvd);
-    },
-    dataType: "text",
-    complete: function () {
-        // call a function on complete 
+    var dataPoints = [];
+     
+    var chart = new CanvasJS.Chart("chartContainer", {
+        animationEnabled: true,
+        exportEnabled: true,
+        title: {
+            text: "Homicide"
+        },
+        axisY: {
+            title: ""
+        },
+        data: [{
+            type: "column",
+            toolTipContent: "persons",
+            dataPoints: dataPoints
+        }]
+    });
+     
+    $.get("http://127.0.0.1:5500/crimes.csv", getDataPointsFromCSV);
+     
+    //CSV Format
+    //Year,Volume
+    function getDataPointsFromCSV(csv) {
+        var points;
+        var csvLines = csv.split(/[\r?\n|\r|\n]+/);
+        for (var i = 1; i < csvLines.length; i++) {
+            if (csvLines[i].length > 0) {
+                points = csvLines[i].split(",");
+                dataPoints.push({
+                    label: points[0],
+                    y: parseFloat(points[0])
+                   
+                });
+            }
+        }
+        chart.render();
     }
-});
+     
+    }
+
+window.onload = function() { 
+ 
+    var dataPoints = [];
+     
+    var chart = new CanvasJS.Chart("chartContainer1", {
+        animationEnabled: true,
+        exportEnabled: true,
+        title: {
+            text: "Homicide"
+        },
+        axisY: {
+            title: ""
+        },
+        data: [{
+            type: "column",
+            toolTipContent: "persons",
+            dataPoints: dataPoints
+        }]
+    });
+     
+    $.get("http://127.0.0.1:5500/prison.csv", getDataPointsFromCSV);
+     
+    //CSV Format
+    //Year,Volume
+    function getDataPointsFromCSV(csv) {
+        var points;
+        var csvLines = csv.split(/[\r?\n|\r|\n]+/);
+        for (var i = 1; i < csvLines.length; i++) {
+            if (csvLines[i].length > 0) {
+                points = csvLines[i].split(",");
+                dataPoints.push({
+                    label: points[0],
+                    y: parseFloat(points[1,2])
+                   
+                });
+            }
+        }
+        chart.render();
+    }
+     
+    }
